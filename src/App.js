9879fae973d1183c16aad38ad914f1e45a0ac13e
srojales <stephenrojales@gmail.com>
2023-02-26 00:04:42 +0800

    added initial App and Landing  layout structure using tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import {
+ 
+  UserIcon,
+} from "@heroicons/react/20/solid";
+import Landing from "./components/Landing";
+
 
 function App() {
+
+
+  function handleButtonClick() {
+ 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     
+      <div className="min-h-full">
+        <header className="bg-white shadow ">
+          <div className="lg:flex lg:items-center lg:justify-between  p-5">
+            <div className="min-w-0 flex-1">
+              <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-8 w-8"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                </div>
+                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                  Weather Forcast
+                </h2>
+              </div>
+            </div>
+            <div className="mt-5 flex lg:mt-0 lg:ml-4">
+              <span className="hidden sm:block">
+                <button
+                  onClick={handleButtonClick}
+                  type="button"
+                  className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <UserIcon
+                    className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                    aria-hidden="true"
+                  />
+                  Logout
+                </button>
+              </span>
+            </div>
+          </div>
+          
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      
+            <div className="px-4 py-6 sm:px-0">
+   
+
+            <Landing />
+
+
+            </div>
+     
+          </div>
+        </main>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Landing.js
@@ -0,0 +1,42 @@
+
+import { UserIcon } from "@heroicons/react/24/outline"
+
+
+function Landing() {
+
+    
+    const loginClick =()=>{
+       
+    }   
+
+   return (
+     <>
+       <main className="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
+        <div className="text-center">
+   
+          <h4 className="mt-4 font-bold tracking-tight text-gray-900 sm:text-xl">
+            Welcome to the weather forcast web application. Please login with your Github user to use the application and view the wearther in your city.
+        </h4>
+ 
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+          <button
+                  onClick={loginClick}
+                  type="button"
+                  className="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <UserIcon
+                    className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                    aria-hidden="true"
+                  />
+                  Login
+                </button>
+           
+          </div>
+        </div>
+      </main>
+     </>
+   )
+}
+
+export default Landing
+
